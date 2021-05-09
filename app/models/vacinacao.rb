ea1f9d2ddcf57bdbb6d0392c2a327033c2c2fd38
laisy <https://github.com/laisy>
2021-05-09 18:02:05 -0300

    Criando validates de vacinacao

--- app/models/vacinacao.rb
+++ app/models/vacinacao.rb
@@ -1,2 +1,8 @@
 class Vacinacao < ApplicationRecord
+  has_one :usuario, dependent: :destroy
+  
+  validates :nome_vacina, presence: true, length: {minimum: 3, too_short: "%{count} caracteres é o minimo permitido ", maximum:50, too_long: "%{count} caracteres é o máximo permitido " }
+  validates :descricao_localizacao, presence: true,  length: {minimum: 3, too_short: "%{count} caracteres é o minimo permitido "}
+  validates :data, presence: true
+  validates :horario, presence: true
 end
