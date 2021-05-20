5e950ec80b673527dc9f65c80ae58851077d0e92
laisy <https://github.com/laisy>
2021-05-19 23:18:01 -0300

    Corrigindo model de Usuario

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -1,12 +1,12 @@
 class Usuario < ApplicationRecord
 
-  validates :cpf, presence: true, uniqueness: true,  numericality: { only_integer: true }
+  validates :cpf, presence: true, uniqueness: true,  numericality: { only_integer: true },  length: {is: 11}
   validates :nome, presence: true, length: {minimum: 3, too_short: "%{count} caracteres é o minimo permitido ", maximum: 50, too_long: "%{count} caracteres é o máximo permitido " }
   validates :email, presence: true, uniqueness: true, length: {minimum: 3, too_short: "%{count} caracteres é o minimo permitido ", maximum: 50, too_long: "%{count} caracteres é o máximo permitido " }
   validates :senha, presence: true, length: {minimum: 5}
-  validates :data_nascimento, presence: true
+  validate :validarDataNascimento
   validates :nome_mae, presence: true, length: {minimum: 3, too_short: "%{count} caracteres é o minimo permitido ", maximum: 50, too_long: "%{count} caracteres é o máximo permitido " }
-  validates :telefone, presence: true, numericality: { only_integer: true }, length: {minimum: 9, maximum: 19}
+  validates :telefone, presence: true, numericality: { only_integer: true }, length: {is: 11}
 
   has_one :endereco
   has_many :vacinacao
@@ -16,6 +16,12 @@ class Usuario < ApplicationRecord
         return false
     else
       return true
+    end
+  end
+
+  def validarDataNascimento
+    if data_nascimento.present? && data_nascimento > Date.today
+      errors.add(:data_nascimento, "can't be in the past")
     end
   end
   
