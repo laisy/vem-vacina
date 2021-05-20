e7db6d5fd51843d43074c00bd67c2689eeaeec0d
laisy <https://github.com/laisy>
2021-05-19 23:30:45 -0300

    Arrumando o lenght no model de Endereço

--- app/models/endereco.rb
+++ app/models/endereco.rb
@@ -1,7 +1,7 @@
 class Endereco < ApplicationRecord
   belongs_to :usuario
   
-  validates :cep, presence: true, numericality: { only_integer: true }, length: 8
+  validates :cep, presence: true, numericality: { only_integer: true }, length: {is: 8}
   validates :cidade, presence: true, length: {minimum: 3, too_short: "%{count} caracteres é o minimo permitido ", maximum:50, too_long: "%{count} caracteres é o máximo permitido " }
   validates :bairro, presence: true, length: {minimum: 3, too_short: "%{count} caracteres é o minimo permitido ", maximum:50, too_long: "%{count} caracteres é o máximo permitido " }
   validates :logradouro, presence: true, length: {minimum: 3, too_short: "%{count} caracteres é o minimo permitido ", maximum:50, too_long: "%{count} caracteres é o máximo permitido " }
